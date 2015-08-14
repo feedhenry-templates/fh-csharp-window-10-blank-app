abbc181994244ec9a4f19acd40c9c029e15aa59f
Erik Jan de Wit <erikjan.dewit@gmail.com>
2015-08-14 15:27:44 +0200

    initial version

---
+++ fh-csharp-window-10-blank-app/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using FHSDK;
+using FHSDK81.Phone;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace fh_csharp_window_10_blank_app
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            try
+            {
+                await FHClient.Init();
+            }
+            catch (FHException)
+            {
+
+            }
+        }
+    }
+}
